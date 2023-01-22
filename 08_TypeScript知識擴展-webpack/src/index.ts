f9427fb19d3924f84e0cb8857ed13e3bfd0086d9
allen3_huang@asus.com <allen3_huang@asus.com>
2023-01-22 22:39:16 +0800

    modify tsconfig

--- 08_TypeScript知識擴展-webpack/src/index.ts
+++ 08_TypeScript知識擴展-webpack/src/index.ts
@@ -4,3 +4,12 @@ const message: string = 'Hello World';
 
 console.log(message.length, message);
 console.log(sum(20, 30));
+
+// lib.dom.d.ts
+const h2El = document.createElement('h2');
+h2El.textContent = 'Hello TypeScript';
+document.body.append(h2El);
+
+// lib.es2015.d.ts
+const promise = new Promise((resolve, reject) => {});
+console.log(message.startsWith('Hello'));
